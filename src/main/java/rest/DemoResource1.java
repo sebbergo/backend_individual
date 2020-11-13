c6390c31a26f237dfbab066d33263c53f9d7a7c9
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2020-11-13 15:23:04 +0100

    test af deploy

---
+++ src/main/java/rest/DemoResource1.java
@@ -0,0 +1,45 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entities.User;
+import facades.FacadeExample;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import utils.*;
+
+
+/**
+ * @author lam@cphbusiness.dk
+ */
+@Path("test")
+public class DemoResource1 {
+    
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getInfoForAll() {
+        return "{\"msg\":\"Hello anonymous\"}";
+    }
+
+}
